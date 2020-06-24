5cc81c1efc19bbcc3317b972a9a3f746825eeec5
Stefano <stefano.zaccaria29@gmail.com>
2020-06-24 15:29:44 +0200

    ex cognomi

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 // ESERCIZIO COGNOMI
 
-var surname = ['Rossi', 'Bianco', 'Esposito', 'Russo', 'Zaccaria'];
+// var surname = ['Rossi', 'Bianco', 'Esposito', 'Russo', 'Zaccaria'];
 
-var userSurname = prompt("INSERISCI IL TUO COGNOME");
+// var userSurname = prompt("INSERISCI IL TUO COGNOME");
 
-surname.push(userSurname);
+// surname.push(userSurname);
 
-surname = surname.sort();
+// surname = surname.sort();
 
-console.log(surname);
+// console.log(surname);
 
-var userPosition = surname.indexOf(userSurname);
+// var userPosition = surname.indexOf(userSurname);
 
-console.log(userPosition + 1);
+// console.log(userPosition + 1);
+
+// var nome1 = 'marco';
+// var nome2 = 'luca';
+// var nome3 = 'giovanni';
+// var nome4 = 'matteo';
+// var nome5 = 'leo';
+
+// var nomi = [nome1, nome2, nome3, nome4, nome5];
+
+// console.log(nomi);
+
+// var user = nomi.sort();
+
+// console.log(user);
+
+// var risultato = nome1.localeCompare(nome2);
+
+// console.log(risultato);
