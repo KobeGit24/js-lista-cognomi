2403f0450eacfadb6c809da027381c68eb0650d0
Stefano <stefano.zaccaria29@gmail.com>
2020-06-24 16:44:05 +0200

    ex cognomi!

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 // ESERCIZIO COGNOMI
 
+// FIRST METHOD WITH "FOR"
+
 var surname = ['Rossi', 'Bianco', 'Esposito', 'Russo', 'Zaccaria'];
+
+console.log(surname);
 
 var userSurname = prompt("INSERISCI IL TUO COGNOME");
 
-surname.push(userSurname);
+var user = userSurname.toUpperCase();
+
+surname.push(user);
 
 surname = surname.sort();
 
 console.log(surname);
 
-var userPosition = surname.indexOf(userSurname);
+for (var i = 0; i < surname.length; i++) { 
+    
+    if (surname[i] == user ) {
+        console.log(i + 1);
+    } 
+} 
 
-console.log(userPosition + 1);
+// SECOND METHOD WITHOUT "FOR"
+
+// var surname = ['Rossi', 'Bianco', 'Esposito', 'Russo', 'Zaccaria'];
+
+// console.log(surname);
+
+// var userSurname = prompt("INSERISCI IL TUO COGNOME");
+
+// var user = userSurname.toUpperCase();
+
+// surname.push(user);
+
+// surname = surname.sort();
+
+// console.log(surname);
+
+// var userPosition = surname.indexOf(user);
+
+// console.log(userPosition + 1);
